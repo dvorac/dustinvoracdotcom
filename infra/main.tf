df051299ca81e48c2e2c6871deb532bfb18e3f4b
dvorac <dustin.vorac@gmail.com>
2022-09-04 12:47:43 -0500

    added links to main page, styled to be less black-and-white

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,5 @@
+# following https://medium.com/@jerome.decoster/github-actions-hugo-terraform-s3-bc4609cedb1
+# see: https://github.com/jeromedecoster/github-actions-hugo-terraform-s3
 provider aws {
   region  = var.region
   profile = var.profile
