174cb2185587d22d5e4d77abe3bd98906de6ce53
dvorac <dustin.vorac@gmail.com>
2022-09-14 11:31:31 -0500

    correct errors in plan

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,26 +1,42 @@
-resource aws_s3_bucket bucket {
+# see https://github.com/hashicorp/learn-terraform-cloudflare-static-website/blob/020a13f169198de9a991d0922ef23f16a7ee4351/main.tf
+resource aws_s3_bucket website {
   bucket        = var.bucket
   force_destroy = true
-  acl           = "public-read"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource aws_s3_bucket_website_configuration website {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
-data aws_iam_policy_document policy {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.bucket.arn}/*"]
-  }
+resource aws_s3_bucket_acl website {
+  bucket = aws_s3_bucket.website.id
+  acl = "public-read"
 }
 
-resource aws_s3_bucket_policy bucket_policy {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.policy.json
+resource aws_s3_bucket_policy website {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.website.arn,
+          "${aws_s3_bucket.website.arn}/*",
+        ]
+      },
+    ]
+  })
 }
