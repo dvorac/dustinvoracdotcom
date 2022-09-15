dce8fa3dc068c2479fbc177750804e4bc3d133ca
dvorac <dustin.vorac@gmail.com>
2022-09-15 11:13:08 -0500

    tf fmt

--- infra/route53.tf
+++ infra/route53.tf
@@ -1,26 +1,26 @@
 data "aws_route53_zone" "domain" {
   provider = aws
-  name = "${var.apex_domain}."
+  name     = "${var.apex_domain}."
 }
 
 resource "aws_route53_record" "ipv4" {
   zone_id = data.aws_route53_zone.domain.zone_id
-  name = "www.${var.apex_domain}"
-  type = "A"
-  
+  name    = "www.${var.apex_domain}"
+  type    = "A"
+
   alias {
-    name = aws_cloudfront_distribution.distribution.domain_name
+    name    = aws_cloudfront_distribution.distribution.domain_name
     zone_id = aws_cloudfront_distribution.distribution.hosted_zone_id
   }
 }
 
 resource "aws_route53_record" "ipv6" {
   zone_id = data.aws_route53_zone.domain.zone_id
-  name = "www.${var.apex_domain}"
-  type = "AAAA"
-  
+  name    = "www.${var.apex_domain}"
+  type    = "AAAA"
+
   alias {
-    name = aws_cloudfront_distribution.distribution.domain_name
+    name    = aws_cloudfront_distribution.distribution.domain_name
     zone_id = aws_cloudfront_distribution.distribution.hosted_zone_id
   }
 }
