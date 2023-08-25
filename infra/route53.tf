ace1fb700034c6558703bebe66698fd6cbcf3b8c
dvorac <dustin.vorac@exceleras.com>
2023-08-25 09:49:42 -0500

    readd subdomain

--- infra/route53.tf
+++ infra/route53.tf
@@ -26,3 +26,27 @@ resource "aws_route53_record" "ipv6" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "ipv4" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "exceleras.${var.apex_domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "ipv6" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "exceleras.${var.apex_domain}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
